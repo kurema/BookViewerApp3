8ac43602f8003efe75f16c539f5b71bb0a26abcc
kurema <kurema_makoto_software@yahoo.co.jp>
2016-02-23 17:29:49 +0900

    Better icon.

--- BookViewerApp/Properties/AssemblyInfo.cs
+++ BookViewerApp/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BookViewerApp")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © kurema  2015-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
