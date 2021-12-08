9595b88465028296b3ae38ea606e3b07b0a787bd
kurema <kurema_makoto_software@yahoo.co.jp>
2021-12-08 11:49:43 +0900

    Daily: ViewModel update

--- FileExplorerControl/ViewModels/RenameViewModel.cs
+++ FileExplorerControl/ViewModels/RenameViewModel.cs
@@ -32,7 +32,7 @@ public class RenameViewModel : INotifyPropertyChanged
     }
     #endregion
 
-    public ObservableCollection<FileItemViewModel> Files { get; } = new();
+    public ObservableCollection<RenameItemViewModel> Files { get; } = new();
 
     public async Task LoadFolder(Models.FileItems.IFileItem fileItem)
     {
@@ -42,25 +42,61 @@ public class RenameViewModel : INotifyPropertyChanged
         var files = await fileItem.GetChildren();
         foreach (var item in files)
         {
-            if (item.RenameCommand?.CanExecute("sample.txt") is true) Files.Add(new FileItemViewModel(item));
+            if (item.RenameCommand?.CanExecute("sample.txt") is true) Files.Add(new RenameItemViewModel(item));
         }
     }
-}
 
-public class RenameItemViewModel : INotifyPropertyChanged,INotifyDataErrorInfo
-{
-    public bool HasErrors => throw new NotImplementedException();
 
-    public IEnumerable GetErrors(string propertyName)
+    private bool _IsRegex;
+    public bool IsRegex { get => _IsRegex; set => SetProperty(ref _IsRegex, value); }
+
+
+    private bool _IsCaseSensitive;
+    public bool IsCaseSensitive { get => _IsCaseSensitive; set => SetProperty(ref _IsCaseSensitive, value); }
+
+
+    private bool _MatchAllOccurence;
+    public bool MatchAllOccurence { get => _MatchAllOccurence; set => SetProperty(ref _MatchAllOccurence, value); }
+
+
+    private string _NameOriginal = "";
+    public string NameOriginal { get => _NameOriginal; set => SetProperty(ref _NameOriginal, value); }
+
+    private string _NameRenamed = "";
+
+    public RenameViewModel()
     {
-        throw new NotImplementedException();
+        Files.Add(new RenameItemViewModel(new Models.FileItems.FileItemPlaceHolder()));
     }
 
+    public string NameRenamed { get => _NameRenamed; set => SetProperty(ref _NameRenamed, value); }
 
+}
+
+public class RenameItemViewModel : INotifyPropertyChanged
+{
     private FileItemViewModel _File;
     public FileItemViewModel File { get => _File; set => SetProperty(ref _File, value); }
 
+    private string _NameRenamed;
+    public string NameRenamed { get => _NameRenamed; set => SetProperty(ref _NameRenamed, value); }
 
+    private bool _HasErrors;
+    public bool HasErrors { get => _HasErrors; set => SetProperty(ref _HasErrors, value); }
+
+    private string _ErrorMessage;
+
+    public RenameItemViewModel(Models.FileItems.IFileItem file)
+    {
+        File = new FileItemViewModel(file);
+    }
+
+
+    private bool _IsEnabled = true;
+    public bool IsEnabled { get => _IsEnabled; set => SetProperty(ref _IsEnabled, value); }
+
+
+    public string ErrorMessage { get => _ErrorMessage; set => SetProperty(ref _ErrorMessage, value); }
 
     #region INotifyPropertyChanged
     protected bool SetProperty<T>(ref T backingStore, T value,
@@ -76,7 +112,6 @@ public class RenameItemViewModel : INotifyPropertyChanged,INotifyDataErrorInfo
         return true;
     }
     public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
-    public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
 
     protected void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = "")
     {
