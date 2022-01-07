ecdf64e1c550aec82c72500bd2c0302d74151eec
kurema <kurema_makoto_software@yahoo.co.jp>
2022-01-07 20:01:16 +0900

    Windows.ApplicationModel.Core.CoreApplication.CreateNewView();
    AppWindowにはいくつか問題がある。
    ・ContentDialogが同時に(他ウィンドウと合わせて)1つしか起動しない。
    ・ContentDialogがリサイズされない。

--- FileExplorerControl/Helper/UIHelper.cs
+++ FileExplorerControl/Helper/UIHelper.cs
@@ -26,22 +26,39 @@ public static class UIHelper
 
     public static async Task OpenRename(Models.FileItems.IFileItem file)
     {
-        //Size of ContentDialog has a problem. Not my fault.
-        //var dialog = new ContentDialog()
-        //{
-        //    Content = new Views.RenamePage(),
-        //    FullSizeDesired = true,
-        //};
-        //{
-        //    var loader = Application.ResourceLoader.Loader;
-        //    dialog.CloseButtonText = loader.GetString("Command/OK");
-        //}
-        //await dialog.ShowAsync();
+        {
+            //Size of ContentDialog has a problem. Not my fault.
+            //var dialog = new ContentDialog()
+            //{
+            //    Content = new Views.RenamePage(),
+            //    FullSizeDesired = true,
+            //};
+            //{
+            //    var loader = Application.ResourceLoader.Loader;
+            //    dialog.CloseButtonText = loader.GetString("Command/OK");
+            //}
+            //await dialog.ShowAsync();
+        }
+        {
+            //var newWindow = await Windows.UI.WindowManagement.AppWindow.TryCreateAsync();
+            //var newPage = new Views.RenamePage();
+            //Windows.UI.Xaml.Hosting.ElementCompositionPreview.SetAppWindowContent(newWindow, newPage);
 
-        var newWindow = await Windows.UI.WindowManagement.AppWindow.TryCreateAsync();
-        var newPage = new Views.RenamePage();
-        Windows.UI.Xaml.Hosting.ElementCompositionPreview.SetAppWindowContent(newWindow, newPage);
-
-        await newWindow.TryShowAsync();
+            //await newWindow.TryShowAsync();
+        }
+        {
+            //https://docs.microsoft.com/ja-jp/windows/apps/design/layout/application-view
+            var coreview = Windows.ApplicationModel.Core.CoreApplication.CreateNewView();
+            Windows.UI.ViewManagement.ApplicationView newAppView = null;
+            await coreview.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                newAppView = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView();
+                var newFrame = new Frame();
+                newFrame.Navigate(typeof(Views.RenamePage), file);
+                Window.Current.Content = newFrame;
+                Window.Current.Activate();
+            });
+            await Windows.UI.ViewManagement.ApplicationViewSwitcher.TryShowAsStandaloneAsync(newAppView.Id);
+        }
     }
 }
