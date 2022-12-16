99aab8acdde42f55fbb442d6e8c95138ef1bffc2
kurema <kurema_makoto_software@yahoo.co.jp>
2022-12-16 15:46:22 +0900

    SearchComplition

--- BrowserControl/Helper/SearchComplitions.cs
+++ BrowserControl/Helper/SearchComplitions.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
+#nullable enable
 namespace kurema.BrowserControl.Helper.SearchComplitions;
 public interface ISearchComplition
 {
@@ -16,7 +17,21 @@ public interface ISearchComplition
 
 public static class SearchComplitionManager
 {
-    private static SemaphoreSlim SemaphoreHttp = new(1, 1);
+    private static SemaphoreSlim semaphoreHttp = new(1, 1);
+    private static Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
+
+    private static async Task<string> GetHttp(string url)
+    {
+        await semaphoreHttp.WaitAsync();
+        try
+        {
+            if (string.IsNullOrEmpty(url)) return string.Empty;
+            if (!Uri.TryCreate(url, UriKind.Absolute, out Uri uri)) return string.Empty;
+            return await httpClient.GetStringAsync(uri);
+        }
+        catch { return string.Empty; }
+        finally { semaphoreHttp.Release(); }
+    }
 
     public class SearchComplitionGoogle : ISearchComplition
     {
@@ -24,22 +39,35 @@ public static class SearchComplitionManager
 
         public async Task<IEnumerable<string>> GetComplitions(string word)
         {
-            await SemaphoreHttp.WaitAsync();
+            var lang = System.Globalization.CultureInfo.CurrentCulture.TwoLetterISOLanguageName;
+            var response = await GetHttp(string.Format(APIUrl, System.Web.HttpUtility.UrlEncode(word), lang));
+            if (string.IsNullOrWhiteSpace(response)) return Array.Empty<string>();
             var result = new List<string>();
             try
             {
+                var xdoc = new System.Xml.XmlDocument();
+                xdoc.LoadXml(response);
+                foreach (var item in xdoc.ChildNodes)
+                {
+                    if (item is not System.Xml.XmlElement itemXe) continue;
+                    if (!itemXe.Name.Equals("CompleteSuggestion", StringComparison.InvariantCultureIgnoreCase)) continue;
+                    if (!itemXe.FirstChild.Name.Equals("suggestion", StringComparison.InvariantCultureIgnoreCase)) continue;
+                    if (itemXe.FirstChild.Attributes.Count != 1) continue;
+                    result.Add(itemXe.FirstChild.Attributes[0].Value);
+                }
+
+                var nodelist = xdoc.SelectNodes("/CompleteSuggestion/suggestion");
+                foreach(var item in nodelist)
+                {
+                    if (item is not System.Xml.XmlElement itemXe) continue;
+                }
             }
             catch
             {
-
-            }
-            finally
-            {
-                SemaphoreHttp.Release();
+                return Array.Empty<string>();
             }
             return result.ToArray();
         }
     }
-
 }
 
