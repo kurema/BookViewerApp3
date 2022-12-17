afe23f7cd454eae4d61897bca75cb89f4a4e478d
kurema <kurema_makoto_software@yahoo.co.jp>
2022-12-17 15:51:43 +0900

    SearchComplitions...

--- BrowserControl/Helper/SearchComplitions.cs
+++ BrowserControl/Helper/SearchComplitions.cs
@@ -39,12 +39,13 @@ public static class SearchComplitionManager
 
         public async Task<IEnumerable<string>> GetComplitions(string word)
         {
-            var lang = System.Globalization.CultureInfo.CurrentCulture.TwoLetterISOLanguageName;
-            var response = await GetHttp(string.Format(APIUrl, System.Web.HttpUtility.UrlEncode(word), lang));
-            if (string.IsNullOrWhiteSpace(response)) return Array.Empty<string>();
-            var result = new List<string>();
             try
             {
+                var lang = System.Globalization.CultureInfo.CurrentCulture.TwoLetterISOLanguageName;
+                var response = await GetHttp(string.Format(APIUrl, System.Web.HttpUtility.UrlEncode(word), lang));
+                if (string.IsNullOrWhiteSpace(response)) return Array.Empty<string>();
+                var result = new List<string>();
+
                 var xdoc = new System.Xml.XmlDocument();
                 xdoc.LoadXml(response);
                 foreach (var item in xdoc.ChildNodes)
@@ -53,20 +54,90 @@ public static class SearchComplitionManager
                     if (!itemXe.Name.Equals("CompleteSuggestion", StringComparison.InvariantCultureIgnoreCase)) continue;
                     if (!itemXe.FirstChild.Name.Equals("suggestion", StringComparison.InvariantCultureIgnoreCase)) continue;
                     if (itemXe.FirstChild.Attributes.Count != 1) continue;
-                    result.Add(itemXe.FirstChild.Attributes[0].Value);
+                    result.Add(itemXe.FirstChild.Attributes["data"].Value);
                 }
 
-                var nodelist = xdoc.SelectNodes("/CompleteSuggestion/suggestion");
-                foreach(var item in nodelist)
-                {
-                    if (item is not System.Xml.XmlElement itemXe) continue;
-                }
+                //var nodelist = xdoc.SelectNodes("/CompleteSuggestion/suggestion");
+                //foreach(var item in nodelist)
+                //{
+                //    if (item is not System.Xml.XmlElement itemXe) continue;
+                //    if (itemXe.FirstChild.Attributes.Count != 1) continue;
+                //    result.Add(itemXe.FirstChild.Attributes["data"].Value);
+                //}
+                return result.ToArray();
             }
             catch
             {
                 return Array.Empty<string>();
             }
-            return result.ToArray();
+        }
+    }
+
+    public class SearchComplitionYahoo : ISearchComplition
+    {
+        public string APIUrl => "http://ff.search.yahoo.com/gossip?output=xml&command={0}";
+
+        public async Task<IEnumerable<string>> GetComplitions(string word)
+        {
+            try
+            {
+                var response = await GetHttp(string.Format(APIUrl, System.Web.HttpUtility.UrlEncode(word)));
+                if (string.IsNullOrWhiteSpace(response)) return Array.Empty<string>();
+                var result = new List<string>();
+
+                var xdoc = new System.Xml.XmlDocument();
+                xdoc.LoadXml(response);
+                var nodelist = xdoc.SelectNodes("/m/s");
+                foreach (var item in nodelist)
+                {
+                    if (item is not System.Xml.XmlElement itemXe) continue;
+                    if (itemXe.FirstChild.Attributes.Count == 0) continue;
+                    result.Add(itemXe.FirstChild.Attributes["k"].Value);
+                }
+                return result.ToArray();
+            }
+            catch
+            {
+                return Array.Empty<string>();
+            }
+        }
+    }
+
+    public class SearchComplitionBing : ISearchComplition
+    {
+        public string APIUrl => "https://api.bing.com/osjson.aspx?market={1}&query={0}";
+
+        public async Task<IEnumerable<string>> GetComplitions(string word)
+        {
+            try
+            {
+                var lang = System.Globalization.CultureInfo.CurrentCulture.Name; //such as "en-us".
+                var response = await GetHttp(string.Format(APIUrl, System.Web.HttpUtility.UrlEncode(word), lang));
+                if (string.IsNullOrWhiteSpace(response)) return Array.Empty<string>();
+                var result = new List<string>();
+
+                var document = System.Text.Json.JsonDocument.Parse(response);
+                foreach (var element in document.RootElement.EnumerateArray())
+                {
+                    if (element.ValueKind is System.Text.Json.JsonValueKind.Array)
+                    {
+                        foreach (var item in element.EnumerateArray())
+                        {
+                            if (item.ValueKind is System.Text.Json.JsonValueKind.String)
+                            {
+                                var text = item.GetString();
+                                if (text is not null) result.Add(text);
+                            }
+                        }
+                        break;
+                    }
+                }
+                return result.ToArray();
+            }
+            catch
+            {
+                return Array.Empty<string>();
+            }
         }
     }
 }
