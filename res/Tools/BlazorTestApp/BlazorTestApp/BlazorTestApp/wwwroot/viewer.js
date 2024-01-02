4f25fde7d9f330036a6c650242560b0651db601b
kurema <kurema_makoto_software@yahoo.co.jp>
2024-01-03 01:44:43 +0900

    Bump pdfjs.zip / Update viewer.ts

--- res/Tools/BlazorTestApp/BlazorTestApp/BlazorTestApp/wwwroot/viewer.js
+++ res/Tools/BlazorTestApp/BlazorTestApp/BlazorTestApp/wwwroot/viewer.js
@@ -34,3 +34,50 @@ class Page {
         this.IsLoading = false;
     }
 }
+var PageStates;
+(function (PageStates) {
+    PageStates[PageStates["Full"] = 0] = "Full";
+    PageStates[PageStates["LeftHalf"] = 1] = "LeftHalf";
+    PageStates[PageStates["RightHalf"] = 2] = "RightHalf";
+})(PageStates || (PageStates = {}));
+var PageModes;
+(function (PageModes) {
+    PageModes[PageModes["Simple"] = 0] = "Simple";
+    PageModes[PageModes["AutoDetect"] = 1] = "AutoDetect";
+    PageModes[PageModes["Spread"] = 2] = "Spread";
+    PageModes[PageModes["Portrait"] = 3] = "Portrait";
+})(PageModes || (PageModes = {}));
+var PageDirections;
+(function (PageDirections) {
+    PageDirections[PageDirections["Left"] = 0] = "Left";
+    PageDirections[PageDirections["Right"] = 1] = "Right";
+    PageDirections[PageDirections["Down"] = 2] = "Down";
+})(PageDirections || (PageDirections = {}));
+class PageCombinationEntry {
+    constructor(page, pageState) {
+        this.Page = page;
+        this.PageState = pageState;
+    }
+}
+class Vector2 {
+    constructor(x, y) {
+        this.X = x;
+        this.Y = y;
+    }
+    Reset() {
+        this.X = 0;
+        this.Y = 0;
+    }
+    Set(x, y) {
+        this.X = x;
+        this.Y = y;
+    }
+}
+class ZoomInformation {
+    constructor(centerX, centerY, factor = 1.0) {
+        //This vector is relative to height of image.
+        this.Center = new Vector2(centerX, centerY);
+        this.Factor = factor;
+        this.FactorInitial = factor;
+    }
+}
--- res/Tools/BlazorTestApp/BlazorTestApp/BlazorTestApp/wwwroot/viewer.ts
+++ res/Tools/BlazorTestApp/BlazorTestApp/BlazorTestApp/wwwroot/viewer.ts
@@ -13,7 +13,7 @@
         this.Image = null;
     }
 
-    async Load() {
+    async Load(): Promise<void> {
         await new Promise<void>((resolve, reject) => {
             if (this.IsLoaded) {
                 resolve();
@@ -37,9 +37,77 @@
     get Width(): number | null { return this.Image?.width; }
     get Height(): number | null { return this.Image?.height; }
 
-    Free() {
+    Free(): void{
         this.Image = null;
         this.IsLoaded = false;
         this.IsLoading = false;
     }
+}
+
+enum PageStates {
+    Full,
+    LeftHalf,
+    RightHalf
+}
+
+enum PageModes {
+    Simple,
+    AutoDetect,
+    Spread,
+    Portrait,
+}
+
+enum PageDirections {
+    Left,
+    Right,
+    Down,
+}
+
+class PageCombinationEntry {
+    Page: Page;
+    PageState: PageStates;
+
+    constructor(page: Page, pageState: PageStates) {
+        this.Page = page;
+        this.PageState = pageState;
+    }
+}
+
+class Vector2 {
+    X: number;
+    Y: number;
+
+    constructor(x: number, y: number) {
+        this.X = x;
+        this.Y = y;
+    }
+
+    Reset(): void{
+        this.X = 0;
+        this.Y = 0;
+    }
+
+    Set(x, y): void {
+        this.X = x;
+        this.Y = y;
+    }
+}
+
+class ZoomInformation {
+    Center: Vector2;
+    Factor: number;
+    FactorInitial: number;
+
+    constructor(centerX: number, centerY: number, factor = 1.0) {
+        //This vector is relative to height of image.
+        this.Center = new Vector2(centerX, centerY);
+        this.Factor = factor;
+        this.FactorInitial = factor;
+    }
+}
+
+interface IDrawOption {
+    get Opacity(): number;
+    get Factor(): number;
+    get Shift(): Vector2;
 }
