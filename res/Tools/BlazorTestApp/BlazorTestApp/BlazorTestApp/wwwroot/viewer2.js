43dae89c5b5cc5dbf40372af4776d34fa440bdec
kurema <kurema_makoto_software@yahoo.co.jp>
2024-01-10 02:24:08 +0900

    Init viewer.ts / DirectoryInfo.d.ts seems to be ignored.

--- res/Tools/BlazorTestApp/BlazorTestApp/BlazorTestApp/wwwroot/viewer2.js
+++ res/Tools/BlazorTestApp/BlazorTestApp/BlazorTestApp/wwwroot/viewer2.js
@@ -1,2 +1,85 @@
+class Vector2d {
+    constructor(x = 0, y = 0) {
+        this.X = x;
+        this.Y = y;
+    }
+}
+class Size2d {
+    constructor(width = 0, height = 0) {
+        this.Width = width;
+        this.Height = height;
+    }
+}
+var PageStates;
+(function (PageStates) {
+    PageStates[PageStates["Full"] = 0] = "Full";
+    PageStates[PageStates["LeftHalf"] = 1] = "LeftHalf";
+    PageStates[PageStates["RightHalf"] = 2] = "RightHalf";
+})(PageStates || (PageStates = {}));
+var PageModes;
+(function (PageModes) {
+    PageModes[PageModes["Simple"] = 0] = "Simple";
+    PageModes[PageModes["AutoDetect"] = 1] = "AutoDetect";
+    PageModes[PageModes["Spread"] = 2] = "Spread";
+    PageModes[PageModes["Portrait"] = 3] = "Portrait";
+})(PageModes || (PageModes = {}));
+var PageDirections;
+(function (PageDirections) {
+    PageDirections[PageDirections["Left"] = 0] = "Left";
+    PageDirections[PageDirections["Right"] = 1] = "Right";
+    PageDirections[PageDirections["Down"] = 2] = "Down";
+})(PageDirections || (PageDirections = {}));
+class CursorState {
+    constructor() {
+    }
+}
 class Page {
+    constructor(image) {
+        this.Path = image.folder + "/" + image.name;
+        this.Data = image;
+        this._IsLoading = false;
+        this._IsLoaded = false;
+        this.Image = null;
+        this._LoadedActions = [];
+        this._Size = null;
+        this.HasError = false;
+    }
+    async Load() {
+        await new Promise((resolve, reject) => {
+            if (this._IsLoaded) {
+                resolve();
+                return;
+            }
+            else if (this._IsLoading) {
+                this._LoadedActions.push(new function () { resolve(); });
+                return;
+            }
+            this._IsLoading = true;
+            const img = new Image();
+            img.src = this.Path;
+            img.onerror = () => {
+                this.HasError = true;
+                reject();
+            };
+            img.onload = () => {
+                this.HasError = false;
+                this.Image = img;
+                this._IsLoaded = true;
+                this._IsLoading = false;
+                this._LoadedActions.forEach(f => f());
+                this._LoadedActions = [];
+                this._Size = new Size2d(img.width, img.height);
+                resolve();
+            };
+        });
+    }
+    get Size() { var _a; return (_a = this._Size) !== null && _a !== void 0 ? _a : new Size2d(); }
+    get SizeLoaded() { return this.Size === null; }
+    Free() {
+        this.Image = null;
+        this._IsLoaded = false;
+        this._IsLoading = false;
+    }
+}
+class Book {
 }
--- res/Tools/BlazorTestApp/BlazorTestApp/BlazorTestApp/wwwroot/viewer2.ts
+++ res/Tools/BlazorTestApp/BlazorTestApp/BlazorTestApp/wwwroot/viewer2.ts
@@ -1,3 +1,116 @@
-class Page {
+interface JsonImageEntry {
+    folder?: string;
+    name?: string;
+    isFolder?: boolean;
+    size?: number;
+    updated?: string;
+    [k: string]: unknown;
+}
 
+class Vector2d {
+    X: number;
+    Y: number;
+
+    constructor(x: number = 0, y: number = 0) {
+        this.X = x;
+        this.Y = y;
+    }
+}
+
+class Size2d {
+    Width: number;
+    Height: number;
+
+    constructor(width: number = 0, height: number = 0) {
+        this.Width = width;
+        this.Height = height;
+    }
+}
+
+enum PageStates {
+    Full,
+    LeftHalf,
+    RightHalf
+}
+
+enum PageModes {
+    Simple,
+    AutoDetect,
+    Spread,
+    Portrait,
+}
+
+enum PageDirections {
+    Left,
+    Right,
+    Down,
+}
+
+class CursorState {
+    CurrentPosition: Vector2d;
+
+    constructor() {
+    }
+}
+
+class Page {
+    Path: string;
+    Data: JsonImageEntry;
+    private _IsLoading: boolean;
+    private _IsLoaded: boolean;
+    private _LoadedActions: Function[];
+    private _Size: Size2d | null;
+    Image: HTMLImageElement | null;
+    HasError: boolean;
+
+    constructor(image: JsonImageEntry) {
+        this.Path = image.folder + "/" + image.name;
+        this.Data = image;
+        this._IsLoading = false;
+        this._IsLoaded = false;
+        this.Image = null;
+        this._LoadedActions = [];
+        this._Size = null;
+        this.HasError = false;
+    }
+
+    async Load(): Promise<void> {
+        await new Promise<void>((resolve, reject) => {
+            if (this._IsLoaded) {
+                resolve();
+                return;
+            } else if (this._IsLoading) {
+                this._LoadedActions.push(new function () { resolve() });
+                return;
+            }
+            this._IsLoading = true;
+            const img = new Image();
+            img.src = this.Path;
+            img.onerror = () => {
+                this.HasError = true;
+                reject();
+            };
+            img.onload = () => {
+                this.HasError = false;
+                this.Image = img;
+                this._IsLoaded = true;
+                this._IsLoading = false;
+                this._LoadedActions.forEach(f => f());
+                this._LoadedActions = [];
+                this._Size = new Size2d(img.width, img.height);
+                resolve();
+            }
+        });
+    }
+
+    get Size() { return this._Size ?? new Size2d(); }
+    get SizeLoaded() { return this.Size === null; }
+
+    Free(): void {
+        this.Image = null;
+        this._IsLoaded = false;
+        this._IsLoading = false;
+    }
+}
+class Book {
 }
