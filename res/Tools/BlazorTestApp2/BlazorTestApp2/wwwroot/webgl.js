61b16313e61db9687a24aaf9147dfe8f4e397d41
kurema <kurema_makoto_software@yahoo.co.jp>
2024-06-01 01:52:36 +0900

    Bump pdf.js / Update webgl.ts

--- res/Tools/BlazorTestApp2/BlazorTestApp2/wwwroot/webgl.js
+++ res/Tools/BlazorTestApp2/BlazorTestApp2/wwwroot/webgl.js
@@ -1,6 +1,4 @@
 class CanvasManager {
-    //private VertexShaders: { [key: string]: WebGLShader; };
-    //private FragmentShaders: { [key: string]: WebGLShader; };
     Setup(canvas) {
         this.Canvas = canvas;
         if (!canvas)
@@ -8,8 +6,6 @@ class CanvasManager {
         this.Context = canvas.getContext("webgl2");
         if (!this.Context)
             throw new Error("webgl2 is not supported.");
-        //    this.VertexShaders = {};
-        //    this.FragmentShaders = {};
     }
     InitShader(
     //name: string,
@@ -24,14 +20,6 @@ class CanvasManager {
         gl.compileShader(shader);
         if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
             throw new Error(`Failed to compile shader: ${gl.getShaderInfoLog(shader)}`);
-        //switch (type) {
-        //    case "VERTEX_SHADER":
-        //        this.VertexShaders[name] = shader;
-        //        break;
-        //    case "FRAGMENT_SHADER":
-        //        this.FragmentShaders[name] = shader;
-        //        break;
-        //}
         return shader;
     }
 }
@@ -50,17 +38,20 @@ class Program {
         this.vertexShader = this.InitShader(webgl, 'VERTEX_SHADER', `
     attribute vec4 a_position;
     //attribute vec2 a_texCoord;
-    //varying vec2 vTexCoord;
+    varying vec2 vTexCoord;
 
     void main() {
       gl_Position = a_position;
-      //vTexCoord=vec2(0.5,0.5);
+      vTexCoord=vec2(a_position.x,a_position.y);
     }
   `);
+        //https://stackoverflow.com/questions/27058064/why-do-i-need-to-define-a-precision-value-in-webgl-shaders
         this.fragmentShader = this.InitShader(webgl, 'FRAGMENT_SHADER', `
-        //varying vec2 vTexCoord;
+        precision highp float;
+        varying vec2 vTexCoord;
     void main() {
-      gl_FragColor = vec4(0.1, 0, 0, 1);
+      gl_FragColor = vec4(vTexCoord.x, vTexCoord.y, (1.0-vTexCoord.x)*(1.0-vTexCoord.y)/2.0, 1);
+      //gl_FragColor = vec4(vTexCoord.x, vTexCoord.y, 1.0-(pow(vTexCoord.x,2.0)+pow(vTexCoord.y,2.0))/2.0, 1);
     }
 
     `);
--- res/Tools/BlazorTestApp2/BlazorTestApp2/wwwroot/webgl.ts
+++ res/Tools/BlazorTestApp2/BlazorTestApp2/wwwroot/webgl.ts
@@ -1,16 +1,12 @@
 class CanvasManager {
     Canvas: HTMLCanvasElement;
     private Context: WebGL2RenderingContext;
-    private VertexShaders: { [key: string]: WebGLShader; };
-    private FragmentShaders: { [key: string]: WebGLShader; };
 
     Setup(canvas: HTMLCanvasElement) {
         this.Canvas = canvas;
         if (!canvas) throw new Error("canvas not found!");
         this.Context = canvas.getContext("webgl2");
         if (!this.Context) throw new Error("webgl2 is not supported.");
-        this.VertexShaders = {};
-        this.FragmentShaders = {};
     }
 
     InitShader(
@@ -28,14 +24,6 @@ class CanvasManager {
 
         if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) throw new Error(`Failed to compile shader: ${gl.getShaderInfoLog(shader)}`);
 
-        switch (type) {
-            case "VERTEX_SHADER":
-                this.VertexShaders[name] = shader;
-                break;
-            case "FRAGMENT_SHADER":
-                this.FragmentShaders[name] = shader;
-                break;
-        }
         return shader;
     }
 }
@@ -55,17 +43,20 @@ class Program {
         this.vertexShader = this.InitShader(webgl, 'VERTEX_SHADER', `
     attribute vec4 a_position;
     //attribute vec2 a_texCoord;
-    //varying vec2 vTexCoord;
+    varying vec2 vTexCoord;
 
     void main() {
       gl_Position = a_position;
-      //vTexCoord=vec2(0.5,0.5);
+      vTexCoord=vec2(a_position.x,a_position.y);
     }
   `);
+        //https://stackoverflow.com/questions/27058064/why-do-i-need-to-define-a-precision-value-in-webgl-shaders
         this.fragmentShader = this.InitShader(webgl, 'FRAGMENT_SHADER', `
-        //varying vec2 vTexCoord;
+        precision highp float;
+        varying vec2 vTexCoord;
     void main() {
-      gl_FragColor = vec4(0.1, 0, 0, 1);
+      gl_FragColor = vec4(vTexCoord.x, vTexCoord.y, (1.0-vTexCoord.x)*(1.0-vTexCoord.y)/2.0, 1);
+      //gl_FragColor = vec4(vTexCoord.x, vTexCoord.y, 1.0-(pow(vTexCoord.x,2.0)+pow(vTexCoord.y,2.0))/2.0, 1);
     }
 
     `);
